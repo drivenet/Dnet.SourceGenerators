dea0d7d24510bc7d1277763371ff1b1b393b1a25
Aristarkh Zagorodnikov <xm@x-infinity.com>
2025-04-16 10:59:32 +0100

    feat: add MethodTarget

---
+++ src/Dnet.SourceGenerators/MethodTarget.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Dnet.SourceGenerators;
+
+/// <summary>
+///     A generator target for any method declaration.
+/// </summary>
+///
+/// <param name="Declaration">The method declaration.</param>
+/// <param name="Method">The method symbol.</param>
+public record MethodTarget(
+    MethodDeclarationSyntax Declaration,
+    IMethodSymbol Method)
+    : DeclaredSymbolTarget<MethodDeclarationSyntax, IMethodSymbol>(Declaration, Method)
+{
+}
